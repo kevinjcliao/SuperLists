2f6436afdc506215990a22733a4bcf56bd174f58
Kevin Jiah-Chih Liao <me@liaokev.in>
2015-01-14 19:03:45 +0800

    Added to home template and resolved import problem in tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
-
+from django.template.loader import render_to_string
 
 class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
