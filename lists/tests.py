c1a23ee2b1fed7c869ba37ed1438d74d2452983a
Kevin Jiah-Chih Liao <me@liaokev.in>
2015-02-13 16:35:30 -0500

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -24,12 +25,30 @@ class SmokeTest(TestCase):
 
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
-            'home.html',
-            {'new_item_text':  'A new list item'}
-        )
+                'home.html',
+                {'new_item_text':  'A new list item'}
+                )
         self.assertEqual(response.content.decode(), expected_html)
 
 #    def test_home_page_only_saves_items_when_necessary(self):
 #        request = HttpRequest()
 #        home_page(request)
 #        self.assertEqual(Item.objects.count(), 0)
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self): 
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save() 
+
+        second_item = Item()
+        second_item.text= "Item the second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
